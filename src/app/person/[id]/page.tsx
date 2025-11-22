b171fb617b9a29002d720faabc501b811edf16e5
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2025-11-22 12:52:22 -0600

    gift border color changes based on status

--- src/app/person/[id]/page.tsx
+++ src/app/person/[id]/page.tsx
@@ -48,12 +48,34 @@ export default async function Person({ params }: { params: Promise<{ id: string 
                 />
             </h2>
 
+            <div className={styles.statusIndex}>
+                <div className={styles.statusItem}>
+                    <div className={`${styles.statusDot} ${styles.statusWrapped}`}></div> 
+                    <div>Wrapped</div>
+                </div>
+                <div className={styles.statusItem}>
+                    <div className={`${styles.statusDot} ${styles.statusPurchased}`}></div> 
+                    <div>Purchased</div>
+                </div>
+                <div className={styles.statusItem}>
+                    <div className={`${styles.statusDot} ${styles.statusNotPurchased}`}></div> 
+                    <div>Not Purchased</div>
+                </div>
+            </div>
+
             {person.gifts && person.gifts.length > 0 ? (
                 <ul className={styles.giftList}>
                     {person.gifts?.map((gift: any) => (
-                        <li key={gift.id} className={styles.giftListItem}>
+                        <li 
+                            key={gift.id} 
+                            className={`${styles.giftListItem} ${
+                                gift.status === 'wrapped' ? styles.giftWrapped :
+                                gift.status === 'purchased' ? styles.giftPurchased :
+                                styles.giftNotPurchased
+                            }`}
+                        >
                             <Link href={`/gift/${gift.id}`} className={styles.giftLink}>
-                                {gift.name} - {gift.status}
+                                {gift.name}
                             </Link>
                             <EditButton 
                                 itemType='gift'
