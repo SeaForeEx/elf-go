2112a723c7ef0ad68b01f1987b5c597fbddab49e
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2026-01-20 13:59:17 -0600

    Father's Day added to the occasions

--- src/lib/occasions/monthlyOccasions.ts
+++ src/lib/occasions/monthlyOccasions.ts
@@ -1,11 +1,20 @@
 const today = new Date()
 
 function getMothersDayDate(year: number) {
-    const mayFirstDay = new Date(year, 4, 1).getDay()
-    const daysUntilSunday = mayFirstDay === 0 ? 0 : 7 - mayFirstDay
+    const firstDayMay = new Date(year, 4, 1).getDay()
+    const daysUntilSunday = firstDayMay === 0 ? 0 : 7 - firstDayMay
     const mothersDay = new Date(year, 4, 8 + daysUntilSunday)
 
     return mothersDay
+}
+
+function getFathersDayDate(year: number) {
+    const firstDayJune = new Date(year, 5, 1).getDay()
+    const daysUntilSunday = firstDayJune === 0 ? 0 : 7 - firstDayJune
+    const fathersDay = new Date(year, 5, 15 + daysUntilSunday)
+    console.log("father's day: ", fathersDay)
+
+    return fathersDay
 }
 
 export const monthlyOccasions = [
@@ -19,6 +28,12 @@ export const monthlyOccasions = [
         date: getMothersDayDate(today.getFullYear()),
         name: 'Mother\'s Day',
         emoji: '💐',
+        daysUntil: Math.ceil((Number(getMothersDayDate(today.getFullYear())) - Number(today)) / (1000 * 60 * 60 * 24))
+    },
+    {
+        date: getFathersDayDate(today.getFullYear()),
+        name: 'Father\'s Day',
+        emoji: '🛠️',
         daysUntil: Math.ceil((Number(getMothersDayDate(today.getFullYear())) - Number(today)) / (1000 * 60 * 60 * 24))
     },
     {
