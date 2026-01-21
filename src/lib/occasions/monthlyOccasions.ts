360b824331db4a1ed36c7c60eddadd060ec7e1d5
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2026-01-20 22:15:16 -0600

    Added Thanksgiving to Footer Countdowns

--- src/lib/occasions/monthlyOccasions.ts
+++ src/lib/occasions/monthlyOccasions.ts
@@ -12,9 +12,16 @@ function getFathersDayDate(year: number) {
     const firstDayJune = new Date(year, 5, 1).getDay()
     const daysUntilSunday = firstDayJune === 0 ? 0 : 7 - firstDayJune
     const fathersDay = new Date(year, 5, 15 + daysUntilSunday)
-    console.log("father's day: ", fathersDay)
 
     return fathersDay
+}
+
+function getThanksgivingDate(year: number) {
+    const firstDayNovember = new Date(year, 10, 1).getDay()
+    const daysUntilThursday = (4 - firstDayNovember + 7) % 7
+    const thanksgiving = new Date(year, 10, 22 + daysUntilThursday)
+
+    return thanksgiving
 }
 
 export const monthlyOccasions = [
@@ -34,7 +41,13 @@ export const monthlyOccasions = [
         date: getFathersDayDate(today.getFullYear()),
         name: 'Father\'s Day',
         emoji: '🛠️',
-        daysUntil: Math.ceil((Number(getMothersDayDate(today.getFullYear())) - Number(today)) / (1000 * 60 * 60 * 24))
+        daysUntil: Math.ceil((Number(getFathersDayDate(today.getFullYear())) - Number(today)) / (1000 * 60 * 60 * 24))
+    },
+    {
+        date: getThanksgivingDate(today.getFullYear()),
+        name: 'Thanksgiving',
+        emoji: '🦃',
+        daysUntil: Math.ceil((Number(getThanksgivingDate(today.getFullYear())) - Number(today)) / (1000 * 60 * 60 * 24))
     },
     {
         date: new Date(today.getFullYear(), 11, 25),
